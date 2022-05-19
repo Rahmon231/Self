6ae1d22f0d3de3c202970a04057aa66a25446e09
Akanbi Rahmon <Rahmon.Akanbi@outlook.com>
2022-05-19 22:03:48 +0100

    signup class setup

--- app/src/main/java/com/lemzeeyyy/self/LoginActivity.java
+++ app/src/main/java/com/lemzeeyyy/self/LoginActivity.java
@@ -5,21 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private Button login, signup;
+    private EditText password;
+    private AutoCompleteTextView email;
+    private ProgressBar loginProgress;
+    //Firebase connection
+    private FirebaseFirestore firestore = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email = findViewById(R.id.login_email);
+        password = findViewById(R.id.login_password);
+        loginProgress = findViewById(R.id.login_progress);
         login = findViewById(R.id.log_in_btn);
         signup = findViewById(R.id.sign_up_btn);
         login.setOnClickListener(this::onClick);
         signup.setOnClickListener(this::onClick);
     }
-
     @Override
     public void onClick(View view) {
         switch (view.getId()){
--- app/src/main/java/com/lemzeeyyy/self/SignUpActivity.java
+++ app/src/main/java/com/lemzeeyyy/self/SignUpActivity.java
@@ -1,14 +1,70 @@
 package com.lemzeeyyy.self;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignUpActivity extends AppCompatActivity {
+    private EditText userName, emailSignUp, passwordSignup;
+    private Button signUpBtn;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+    private ProgressBar signUpProgress;
+    //create firebase connection
+    private FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        firebaseAuth = FirebaseAuth.getInstance();
+        userName = findViewById(R.id.user_name);
+        emailSignUp = findViewById(R.id.email_signup);
+        passwordSignup = findViewById(R.id.password_signup);
+        signUpBtn = findViewById(R.id.sign_up_account);
+        signUpProgress = findViewById(R.id.sign_up_progress);
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                currentUser = firebaseAuth.getCurrentUser();
+                if(currentUser!=null){
+                    //user is logged in
+                }else{
+                    //user isnt logged in
+                }
+            }
+        };
+
+        signUpBtn.setOnClickListener(view -> {
+            createUserEmailAccount(email, password, username);
+        });
+    }
+    private void createUserEmailAccount(String email, String password, String username){
+        if(!TextUtils.isEmpty(email)
+        && !TextUtils.isEmpty(password)
+        && !TextUtils.isEmpty(username)){
+
+        }else {
+
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUser = firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
     }
 }
