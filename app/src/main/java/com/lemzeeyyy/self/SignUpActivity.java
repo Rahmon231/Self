c0d71ddbf30cc584ba6d5f4fa06eae8e3f7ac714
Akanbi Rahmon <Rahmon.Akanbi@outlook.com>
2022-05-20 12:06:12 +0100

    postjournal class

--- app/src/main/java/com/lemzeeyyy/self/SignUpActivity.java
+++ app/src/main/java/com/lemzeeyyy/self/SignUpActivity.java
@@ -3,16 +3,30 @@ package com.lemzeeyyy.self;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUpActivity extends AppCompatActivity {
     private EditText userName, emailSignUp, passwordSignup;
@@ -23,6 +37,7 @@ public class SignUpActivity extends AppCompatActivity {
     private ProgressBar signUpProgress;
     //create firebase connection
     private FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = firestore.collection("Users");
 
 
     @Override
@@ -35,26 +50,87 @@ public class SignUpActivity extends AppCompatActivity {
         passwordSignup = findViewById(R.id.password_signup);
         signUpBtn = findViewById(R.id.sign_up_account);
         signUpProgress = findViewById(R.id.sign_up_progress);
-        authStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                currentUser = firebaseAuth.getCurrentUser();
-                if(currentUser!=null){
-                    //user is logged in
-                }else{
-                    //user isnt logged in
-                }
+        authStateListener = firebaseAuth -> {
+            currentUser = firebaseAuth.getCurrentUser();
+            if(currentUser!=null){
+                //user is logged in
+            }else{
+                //user isnt logged in
             }
         };
 
         signUpBtn.setOnClickListener(view -> {
-            createUserEmailAccount(email, password, username);
+            if(!TextUtils.isEmpty(emailSignUp.getText().toString())
+            && !TextUtils.isEmpty(passwordSignup.getText().toString())
+            && !TextUtils.isEmpty(userName.getText().toString())){
+
+                String email = emailSignUp.getText().toString().trim();
+                String password = passwordSignup.getText().toString().trim();
+                String username = userName.getText().toString().trim();
+
+                createUserEmailAccount(email, password, username);
+            }else
+                Toast.makeText(this, "Empty Field not allowed", Toast.LENGTH_SHORT).show();
         });
     }
     private void createUserEmailAccount(String email, String password, String username){
         if(!TextUtils.isEmpty(email)
         && !TextUtils.isEmpty(password)
         && !TextUtils.isEmpty(username)){
+            signUpProgress.setVisibility(View.VISIBLE);
+            firebaseAuth.createUserWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(task -> {
+                        if(task.isSuccessful()){
+                            currentUser = firebaseAuth.getCurrentUser();
+                            String currentUserId = currentUser.getUid();
+                            Map<String, String> userObject = new HashMap<>();
+                            userObject.put("userid",currentUserId);
+                            userObject.put("username",username);
+                            // save to firestore
+                            collectionReference.add(userObject)
+                                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                        @Override
+                                        public void onSuccess(DocumentReference documentReference) {
+                                            documentReference.get()
+                                                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                                if(task.getResult().exists()){
+                                                                    signUpProgress.setVisibility(View.INVISIBLE);
+                                                                    String name = task.getResult()
+                                                                            .getString("username");
+
+                                                                    Intent intent = new Intent(SignUpActivity.this,
+                                                                            PostJournalActivity.class);
+                                                                    intent.putExtra("username",name);
+                                                                    intent.putExtra("userid", currentUserId);
+                                                                    startActivity(intent);
+
+                                                                }else{
+                                                                    signUpProgress.setVisibility(View.INVISIBLE);
+
+                                                                }
+                                                            }
+                                                        });
+                                            }
+
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(SignUpActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                        }else {
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
 
         }else {
 
